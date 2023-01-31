a3a7d7ee179fe5a9bcba213448a3ce08dfe068bd
Conanjoy <119912899+Conanjoy@users.noreply.github.com>
2023-02-01 04:28:46 +0530

    Update Files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 env >> /etc/environment
 
-python3 keep_alive.py &
+python3 keep_alive.py > /proc/1/fd/1 2>/proc/1/fd/2 &
 
-python3 alive.py &
+python3 alive.py > /proc/1/fd/1 2>/proc/1/fd/2 &
 
 echo "$@" &
 
