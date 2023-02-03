d2965e78fa82ee6c289b8c7632d0bc0b74abbc95
Conanjoy <119912899+Conanjoy@users.noreply.github.com>
2023-02-04 00:17:30 +0530

    Update Files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 env >> /etc/environment
 
-nohup python3 keep_alive.py > /proc/1/fd/1 2>/proc/1/fd/2 &
+nohup gunicorn keep_alive:app --bind 0.0.0.0:8080 > /proc/1/fd/1 2>/proc/1/fd/2 &
 
 nohup python3 alive.py > /proc/1/fd/1 2>/proc/1/fd/2 &
 
