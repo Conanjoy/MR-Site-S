e03f70568dd465591584de3b2a2d29bf71006771
Conanjoy <119912899+Conanjoy@users.noreply.github.com>
2023-02-04 08:45:19 +0530

    Update Files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ nohup gunicorn keep_alive:app --bind 0.0.0.0:8080 > /proc/1/fd/1 2>/proc/1/fd/2 
 
 nohup python3 alive.py > /proc/1/fd/1 2>/proc/1/fd/2 &
 
-service "$@" start
+service cron start
+
+exec "@$"
